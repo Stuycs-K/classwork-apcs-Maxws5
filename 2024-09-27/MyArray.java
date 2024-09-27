dd64839db945d4a96a7cd608d3b4a44fa3bb65f2
Maxws5 <mabraham60@stuy.edu>
2024-09-27 11:42:20 -0400

    edit test cases

--- 2024-09-27/MyArray.java
+++ 2024-09-27/MyArray.java
@@ -1,10 +1,10 @@
 public class MyArray{
   public static void main(String[] args)
   {
-    System.out.println("This should output [1, 2, 3, 4]: outputs" + aryToString({1,2,3,4});
-    System.out.println("This should output [Hi, my, name, is, max]: outputs" + aryToString({Hi, my, name, is, max}));
+    System.out.println("This should output [1, 2, 3, 4]: outputs" + aryToString({1;2;3;4});
+    System.out.println("This should output [Hi, my, name, is, max]: outputs" + aryToString({"Hi"; "my"; "name"; "is"; "max"}));
     System.out.println("This should output [0]: outputs" + aryToString({0});
-    System.out.println("This should output []: outputs" + aryToString({});
+    System.out.println("This should output []: outputs" + aryToString({}));
   }
   public static String aryToString(int[] nums)
   {
