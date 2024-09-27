79ad22e85e68a5d22fbce2f1ef7c36f42c6b691d
Maxws5 <mabraham60@stuy.edu>
2024-09-27 11:46:08 -0400

    finished aryToString

--- 2024-09-27/MyArray.java
+++ 2024-09-27/MyArray.java
@@ -1,10 +1,9 @@
 public class MyArray{
   public static void main(String[] args)
   {
-    System.out.println("This should output [1, 2, 3, 4]: outputs" + aryToString({1;2;3;4});
-    System.out.println("This should output [Hi, my, name, is, max]: outputs" + aryToString({"Hi"; "my"; "name"; "is"; "max"}));
-    System.out.println("This should output [0]: outputs" + aryToString({0});
-    System.out.println("This should output []: outputs" + aryToString({}));
+    System.out.println("This should output [1, 2, 3, 4]: outputs" + aryToString(new int[] {1,2,3,4}));
+    System.out.println("This should output [0]: outputs" + aryToString(new int[] {0}));
+    System.out.println("This should output []: outputs" + aryToString(new int[] {}));
   }
   public static String aryToString(int[] nums)
   {
@@ -12,12 +11,12 @@ public class MyArray{
     for (int i = 0; i < nums.length;i++)
     {
       res = res + nums[i];
-      if (i >1)
+      if (i < nums.length-1)
       {
         res = res + ", ";
       }
     }
-    res = res + "]"
+    res = res + "]";
     return res;
   }
 }
