28561032ee7532467c043677a01c86823f443e83
Maxws5 <mabraham60@stuy.edu>
2024-09-27 11:39:49 -0400

    test cases

--- 2024-09-27/MyArray.java
+++ 2024-09-27/MyArray.java
@@ -1,9 +1,12 @@
-public Class MyArray{
+public class MyArray{
   public static void main(String[] args)
   {
-
+    System.out.println("This should output [1, 2, 3, 4]: outputs" + aryToString({1,2,3,4});
+    System.out.println("This should output [Hi, my, name, is, max]: outputs" + aryToString({Hi, my, name, is, max}));
+    System.out.println("This should output [0]: outputs" + aryToString({0});
+    System.out.println("This should output []: outputs" + aryToString({});
   }
-  public static String artToString(int[] nums)
+  public static String aryToString(int[] nums)
   {
     String res = "[";
     for (int i = 0; i < nums.length;i++)
@@ -14,6 +17,7 @@ public Class MyArray{
         res = res + ", ";
       }
     }
+    res = res + "]"
     return res;
   }
 }
