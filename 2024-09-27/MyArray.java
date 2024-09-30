f0d8751a75fa103e4ac47b08e5c589e896c30b0f
Max Abraham <mabraham60@stuy.edu>
2024-09-30 00:39:36 -0400

    Finished with test cases for returnCopy and concatArray

--- 2024-09-27/MyArray.java
+++ 2024-09-27/MyArray.java
@@ -1,9 +1,22 @@
 public class MyArray{
   public static void main(String[] args)
   {
-    System.out.println("This should output [1, 2, 3, 4]: outputs" + aryToString(new int[] {1,2,3,4}));
-    System.out.println("This should output [0]: outputs" + aryToString(new int[] {0}));
-    System.out.println("This should output []: outputs" + aryToString(new int[] {}));
+    System.out.println("THIS IS TESTS FOR aryToString:");
+    System.out.println("This should output [1, 2, 3, -4]: outputs " + aryToString(new int[] {1,2,3,-4}));
+    System.out.println("This should output [0]: outputs " + aryToString(new int[] {0}));
+    System.out.println("This should output [-1, -2, 3]: outputs " + aryToString(new int[] {-1,-2,3}));
+    System.out.println("This should output []: outputs " + aryToString(new int[] {}));
+    System.out.println("\nTHIS IS TESTS FOR returnCopy:");
+    System.out.println("This should output [1, 2, 3, 5, 4]: outputs" + aryToString(new int[] {returnCopy(new int[] {1,2,3,5,4})}));
+    System.out.println("This should output [0]: outputs" + aryToString(new int[] {returnCopy(new int[] {0})}));
+    System.out.println("This should output []: outputs" + aryToString(new int[] {returnCopy(new int[] {})}));
+    System.out.println("This should output [-1, -2, 3]: outputs" + aryToString(new int[] {returnCopy(new int[] {-1,-2,3})}));
+    System.out.println("\nTHIS IS TESTS FOR concatArray:");
+    System.out.println("This should output [1, 2, 3, 2, 3, 4]: outputs" + aryToString(new int[] {concatArray(new int[] {1,2,3},new int[] {2,3,4})}));
+    System.out.println("This should output [-1, 2, -3, 2, 3, -4]: outputs" + aryToString(new int[] {concatArray(new int[] {-1,2,-3},new int[] {2,3,-4})}));
+    System.out.println("This should output []: outputs" + aryToString(new int[] {concatArray(new int[] {},new int[] {})}));
+    System.out.println("This should output [0, 0]: outputs" + aryToString(new int[] {concatArray(new int[] {0},new int[] {0})}));
+    System.out.println("This should output [1, 2, 3, -2, -3, -4]: outputs" + aryToString(new int[] {concatArray(new int[] {1,2,3},new int[] {-2,-3,-4})}));
   }
   public static String aryToString(int[] nums)
   {
@@ -18,5 +31,13 @@ public class MyArray{
     }
     res = res + "]";
     return res;
+  }
+  public static int[] returnCopy(int[]ary)
+  {
+
+  }
+  public static int[] concatArray(int[]ary1,int[]ary2)
+  {
+
   }
 }
