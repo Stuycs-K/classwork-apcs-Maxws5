1d94fe1b5537b184fad8eb4089a2440e3e771086
Maxws5 <mabraham60@stuy.edu>
2024-11-07 11:42:56 -0500

    step 8: comment at the top of Driver

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,6 @@
+/* When calling .speak() with the Bird object, it accesses the overriden version of getName(),
+   instead of the original getName() in the Animal Class. However if you call the .speak() method
+   with an Animal object, it will use the original getName(). */
 public class Driver{
   public static void main(String[] args){
     Animal toucan = new Animal("squack", 5, "Lenny");
