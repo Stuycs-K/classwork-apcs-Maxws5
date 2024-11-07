8ebefd95dbc888f458cfc709361ae7b004098b1f
Maxws5 <mabraham60@stuy.edu>
2024-11-07 11:29:36 -0500

    step 5: tested bird constructor and speak method with bird

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -2,5 +2,7 @@ public class Driver{
   public static void main(String[] args){
     Animal toucan = new Animal("squack", 5, "Lenny");
     toucan.speak();
+    Bird LennyJr = new Bird("SQUACK", 2, "Lenny Jr", 10.5, "rainbow");
+    LennyJr.speak();
   }
 }
