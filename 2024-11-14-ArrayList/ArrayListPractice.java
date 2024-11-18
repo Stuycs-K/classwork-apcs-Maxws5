2e3b5781a2a4fa7a365a86dc746d577f0569266a
Max Abraham <mabraham60@stuy.edu>
2024-11-18 01:29:26 -0500

    made and tested mixLists functiuon

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -35,17 +35,44 @@ public class ArrayListPractice{
     }
     return res;
   }
+  public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+  //return a new ArrayList that has all values of a and b in alternating order that is:
+  //a[0], b[0], a[1], b[1]...
+  //If one list is longer than the other, just attach the remaining values to the end.
+    ArrayList<String> res = new ArrayList<String>(1);
+    int c = 0;
+    if (a.size()>b.size())c = a.size();
+    else c = b.size();
+    for (int i = 0; i<c;i++)
+    {
+      if (i < a.size())
+      {
+        res.add(a.get(i));
+      }
+      if (i < b.size())
+      {
+        res.add(b.get(i));
+      }
+    }
+    return res;
+  }
   public static void main(String[] args)
   {
-    ArrayList<String> test1 = createRandomArray((int)(12*Math.random()));
-    System.out.println(createRandomArray(5));
-    System.out.println(test1);
-    replaceEmpty(test1);
-    System.out.println(test1);
-    System.out.println(makeReversedList(test1));
+    //ArrayList<String> test1 = createRandomArray((int)(12*Math.random()));
+    //ArrayList<String> test3 = createRandomArray((int)(12*Math.random()));
+    //System.out.println(test1);
+    //System.out.println(test3);
+    //System.out.println(mixLists(test1,test3));
+    //System.out.println(createRandomArray(5));
+    //System.out.println(test1);
+    //replaceEmpty(test1);
+    //System.out.println(test1);
+    //System.out.println(makeReversedList(test1));
     ArrayList<String> test2 = createRandomArray(200000);
-    replaceEmpty(test2);
-    System.out.println(makeReversedList(test2));
+    ArrayList<String> test3 = createRandomArray(300000);
+    System.out.println(mixLists(test2,test3));
+    //replaceEmpty(test2);
+    //System.out.println(makeReversedList(test2));
     //System.out.println(test2);
   }
 }
