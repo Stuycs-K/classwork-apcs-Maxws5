9d62396cbfdd3add12c49c9a6ee2cfb7fbe62dd3
Max Abraham <mabraham60@stuy.edu>
2024-11-18 12:28:26 -0500

    finshed advent day 1 part 1

--- 2024-11-18-advent2016/Driver.java
+++ 2024-11-18-advent2016/Driver.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class Driver{
   public static String[] work(String filename){
@@ -10,7 +12,6 @@ public class Driver{
       String temp2 = temp.nextLine();
       System.out.println(temp2);
       String[] input = temp2.split(", ");
-      System.out.println(toString());
       return input;
     }
     catch (FileNotFoundException ex) {
@@ -19,7 +20,28 @@ public class Driver{
       return idk;
     }
   }
+  public static int work2(String[] input){
+    int North = 0;
+    int South = 0;
+    int East = 0;
+    int West = 0;
+    int direct = 0;
+    for (int i = 0; i < input.length; i++){
+      String lor = input[i].substring(0,1);
+      int move = Integer.parseInt(input[i].substring(1));
+      if (lor.equals("R")) direct += 1;
+      else direct -= 1;
+      if (direct==-1) direct = 3;
+      if (direct == 5)direct = 1;
+      if (direct%4==0)North+=move;
+      if (direct%4==1)East+=move;
+      if (direct%4==2)South+=move;
+      if (direct%4==3)West+=move;
+    }
+    int res = Math.abs(North-South) + Math.abs(East-West);
+    return res;
+  }
   public static void main(String[] args){
-    System.out.println(work("advent.txt"));
+    System.out.print(work2(work("advent.txt")));
   }
 }
