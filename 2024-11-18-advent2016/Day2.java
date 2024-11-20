96b10f7aab57d5b49918e5df41eacc00c22ce941
Maxws5 <mabraham60@stuy.edu>
2024-11-20 11:47:21 -0500

    worked on advent day 2 part 1

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 
-public class Day1{
+public class Day2{
   public static String[] work(String filename){
     try {
       File newFile = new File (filename);
       Scanner temp = new Scanner(newFile);
       String temp2 = temp.nextLine();
       //System.out.println(temp2);
-      String[] input = temp2.split(", ");
+      String[] input = temp2.split("\n");
       return input;
     }
     catch (FileNotFoundException ex) {
@@ -19,4 +19,12 @@ public class Day1{
       String[] idk = new String[8];
       return idk;
     }
+    public static String work2(String[] lines){
+      String res = "";
+      String[][] keypad = [[1,2,3],[4,5,6],[7,8,9]];
+      for (int i = 0; i < lines.length;i++){
+        res = res +"1";
+      }
+    }
+    return res;
   }
