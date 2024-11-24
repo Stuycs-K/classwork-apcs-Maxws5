baf3fff8955d1016410fc091ec72c320ea056cf8
Max Abraham <mabraham60@stuy.edu>
2024-11-24 17:48:11 -0500

    finished advent of code day 2 part 2

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -25,27 +25,72 @@ public class Day2{
   }
   public static String work2(ArrayList<String> lines){
     String res = "";
-    String[][] keypad = {{"1"},{"2","3","4"},{"5","6","7","8","9"},{"A","B","C"},{"D"}};
-    int xpos = 1;
-    int ypos = 1;
+    String[][] keypad = {{"0","0","1"},{"0","2","3","4"},{"5","6","7","8","9"},{"0","A","B","C"},{"0","0","D"}};
+    int xpos = 0;
+    int ypos = 2;
+    boolean works = true;
     for (int i = 0; i < lines.size();i++){
-      for (int b = 0; b < lines.get(i).length();b++)
-      {
-        if ((lines.get(i).substring(b,b+1).equals("L"))&&(xpos>0)){
-          xpos--;
-          System.out.println(keypad[ypos][xpos]);
+      for (int b = 0; b < lines.get(i).length();b++){
+        works = true;
+        if ((lines.get(i).substring(b,b+1).equals("L"))){
+          try {
+            "5".equals(keypad[ypos][xpos-1]);
+          }
+          catch (ArrayIndexOutOfBoundsException ex){
+            works = false;
+          }
+          if (works)
+          {
+            if(!("0".equals(keypad[ypos][xpos-1]))){
+              xpos--;
+              System.out.println(keypad[ypos][xpos]);
+            }
+          }
         }
-        if ((lines.get(i).substring(b,b+1).equals("R"))&&(xpos<2)){
-          xpos++;
-          System.out.println(keypad[ypos][xpos]);
+        if ((lines.get(i).substring(b,b+1).equals("R"))){
+          try {
+            "5".equals(keypad[ypos][xpos+1]);
+          }
+          catch (ArrayIndexOutOfBoundsException ex){
+            works = false;
+          }
+          if (works)
+          {
+            if(!("0".equals(keypad[ypos][xpos+1]))){
+              xpos++;
+              System.out.println(keypad[ypos][xpos]);
+            }
+          }
         }
-        if ((lines.get(i).substring(b,b+1).equals("U"))&&(ypos<2)){
-          ypos++;
-          System.out.println(keypad[ypos][xpos]);
+        if ((lines.get(i).substring(b,b+1).equals("U"))){
+          try {
+            "5".equals(keypad[ypos-1][xpos]);
+          }
+          catch (ArrayIndexOutOfBoundsException ex){
+            works = false;
+          }
+          if (works)
+          {
+            if(!("0".equals(keypad[ypos-1][xpos]))){
+              ypos--;
+              System.out.println(keypad[ypos][xpos]);
+            }
+          }
         }
-        if ((lines.get(i).substring(b,b+1).equals("D"))&&(ypos>0)){
-          ypos--;
-          System.out.println(keypad[ypos][xpos]);
+        if ((lines.get(i).substring(b,b+1).equals("D"))){
+          try {
+            "5".equals(keypad[ypos+1][xpos]);
+          }
+          catch (ArrayIndexOutOfBoundsException ex){
+            works = false;
+          }
+          if (works)
+          {
+            if(!("0".equals(keypad[ypos+1][xpos]))){
+              ypos++;
+              System.out.println(keypad[ypos][xpos]);
+            }
+          }
         }
       }
       res = res + keypad[ypos][xpos];
@@ -53,6 +98,11 @@ public class Day2{
     return res;
   }
   public static void main(String[] args){
+    //String[][] test = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
+    //System.out.println(test[0][0]);
+    //System.out.println(test[-1][0]);
+    //System.out.println(test[0][-1]);
+    //System.out.println(test[2][8]);
     System.out.println(work2(work("advent.txt")));
   }
 }
