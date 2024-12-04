bc35de9ebf9dfcecb471a5af263708e1bdccb945
Maxws5 <mabraham60@stuy.edu>
2024-12-04 11:21:23 -0500

    work done in class

---
+++ 2024-12-04-ColorScreen/Driver.java
@@ -0,0 +1,16 @@
+public class Driver{
+  public static final int BLACK = 30;
+  public static final int RED = 31;
+  public static final int GREEN = 32;
+  public static final int YELLOW = 33;
+  public static final int BLUE = 34;
+  public static final int MAGENTA = 35;
+  public static final int CYAN = 36;
+  public static final int WHITE = 37;
+  public static void main(String[] args){
+
+  }
+  public static void border(int color){
+
+  }
+}
